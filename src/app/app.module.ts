ab17ccd3d33abc8b2604c858aa95130389515686
erzhukov <erzhukov@i-teco.ru>
2020-11-13 12:14:55 +0300

    task 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { FourComponent } from './four/four.component';
+import { FiveComponent } from './five/five.component';
+import { SixComponent } from './six/six.component';
+import { SevenComponent } from './seven/seven.component';
+import { EightComponent } from './eight/eight.component';
+import { NineComponent } from './nine/nine.component';
+import { TenComponent } from './ten/ten.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    ItemComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent,
+    FourComponent,
+    FiveComponent,
+    SixComponent,
+    SevenComponent,
+    EightComponent,
+    NineComponent,
+    TenComponent
   ],
   imports: [
     BrowserModule
